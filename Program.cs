fc921273a9a592161ad8900dacdf0f4ea0bbe300
NikitsinIlya <JamsN87@gmail.com>
2023-02-25 00:41:28 +0300

    add programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+void FindInArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    string[] arrayNew = new string[count];
+    int k = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j].Length <= 3) 
+        {
+            arrayNew[k] = array[j];
+            k++;
+        }
+    }
+    Console.WriteLine($"Конечный массив: [{string.Join(", ", arrayNew)}]");
+}
+
+Console.WriteLine("Введите кол-во элементов массива : ");
+int LengthMassive = Convert.ToInt32(Console.ReadLine());
+string[] Massiv = new string[LengthMassive];
+for (int i = 0; i < LengthMassive; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент массива : ");
+    Massiv[i] = Console.ReadLine();   
+}
+Console.WriteLine($"Начальный масссив: [{string.Join(", ", Massiv)}]");
+FindInArray(Massiv);
